dd10b93d79d01e8b78769a7e0688ecd65f671f51
dto <daniel.takashi@gmail.com>
2020-12-20 12:09:00 -0300

    fix(search): fix get function for collections and specific items

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
 
 const wrapItem      = (model, id) => ({model: model, id: id});
-const findItem      = (model, id) => (model && id) ? payload(model).find(item => item.id == id) : void(0);
-const findAll       = (model)     => (model) ? payload(model) : void(0);
-const search        = (model, id) => (model && id) ? findItem(model, id) : findAll(model)//.map((entry) => match(entry));
+const search        = (model, id) => (model && id) ? payload(model).find(item => item.id == id) : payload(model)
 const modelProperty = (item) => Object.keys(item).filter(prop => MODELS.some(model => model == prop))
-
 
 const payload = (model, path) => {
   var payloadDir = !path ? './payloads/' : path
@@ -14,90 +11,78 @@ const payload = (model, path) => {
   return JSON.parse(rawdata)
 }
 
-const folderModels = (path) => {
+const findModels = (path) => {
   var files = []
   const pathJoin = (path, name) => [path, name].join('/');
   
   var folderItems = fs.readdirSync(path)
   var files = folderItems.filter(name => fs.lstatSync(pathJoin(path, name)).isFile())
   var folder = folderItems.filter(name => fs.lstatSync(pathJoin(path, name)).isDirectory())
-  var innerFiles = folder.map(name => folderModels(pathJoin(path, name)))
+  var innerFiles = folder.map(name => findModels(pathJoin(path, name)))
   
   var models = files.concat(innerFiles).flat()
   models = models.filter(filename => filename.match(".json$")).map(name => name.split('.')[0])
   
   return models;
 }
+const MODELS = findModels('./payloads');
 
 const queryPath = (path) => {
-  var f = path.toLowerCase().split('/').filter(i => i)
+  var pathArray = path.toLowerCase().split('/').filter(i => i);
   var query = []
 
-  while(f.length > 0) {
-    var model = f.shift()
-    var id = f.shift()
-    var item = wrapItem(model, id)
-    query.push(item)
-  }
+  while(pathArray.length > 0)
+    query.push(wrapItem(pathArray.shift(), pathArray.shift()))
 
   return query;
 }
 
-const match = (record) => {
-  const entry = record;
-  const fks = Object.keys(record).filter(key => MODELS.some(model => model == key))
+const searchItem = (item) => {
+  var queryResult = search(item.model, item.id)
+  return (queryResult.length > 0) ?
+    queryResult.map(item => resolveForeignKeys(item)) : 
+    resolveForeignKeys(queryResult);
+}
 
-  fks.forEach(model => {
-    entry[model] = entry[model].map(id => match(search(model, id)))
+const resolveForeignKeys = (item) => {
+  var models = modelProperty(item)
+  var foreignKeys = {}
+
+  models.forEach(fk => {
+    foreignKeys[fk] = item[fk].map(id => wrapItem(fk, id))
   })
 
-  return entry
+  Object.keys(foreignKeys).forEach(key => {
+    item[key] = foreignKeys[key].map(item => searchItem(item))
+  })
+
+  return item;
 }
-
-const MODELS = folderModels('./payloads');
-
-
-const objects = (item) => {
-  const normalizeItem = (item) => {
-    var foreignKeys = {}
-    var models = modelProperty(item)
-    models.forEach(fk => {
-      foreignKeys[fk] = item[fk].map(id => wrapItem(fk, id))
-    })
-    for( key in foreignKeys) {
-      if (foreignKeys[key])
-        foreignKeys[key] = foreignKeys[key].map(item => objects(item))
-    }
-
-    for( key in foreignKeys) {
-      if (foreignKeys[key])
-      item[key] = foreignKeys[key]
-    }
-    return item;
-  }
-  
-  var result = search(item.model, item.id)
-
-  return (result.length > 0) ? result.map(item => normalizeItem(item)) : normalizeItem(result);
-}
-
 
 const index = (path) => {
-  try{
-    var queryEntry = queryPath(path)
-    var result = objects(queryEntry.shift())
+  var queryEntry = queryPath(path)
+  var prev = null
+  var lastItem = null
+  var filteredResult = null
+  queryEntry.forEach(item => {
+    lastItem = item
+    var singleItem = search(item.model, item.id)
+    
+    if(prev === null) prev = singleItem;
+    else if(prev[item.model] && prev[item.model].some(id => id == item.id)){
+      prev = singleItem
+    } else {
+      filteredResult = prev[item.model].map(id => wrapItem(item.model, id))
+      filteredResult = filteredResult.map(item => resolveForeignKeys(search(item.model, item.id)))
+      prev = void(0)
+    }
 
-    var curr = result;
-    queryEntry.forEach(item => {
-      var currenIteration = curr[item.model].filter(it => it.id == item.id)
-      //if (currenIteration.length == 0) throw Error("Dont Match")
-      curr[item.model] = currenIteration;
-    })
+  })
 
-    return result
-  } catch (err) {
-    return []
-  }
+  if (filteredResult) return filteredResult;
+  if (prev) return resolveForeignKeys(search(lastItem.model, lastItem.id))
+  return []
 }
+
 
 module.exports = index;
