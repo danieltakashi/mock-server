0cbb14834239dda83a2bc11f2b240b93f97b4be0
dto <daniel.takashi@gmail.com>
2020-12-20 23:50:49 -0300

    feat(persistance): added POST method support

---
+++ controller/post.controller.js
@@ -0,0 +1,77 @@
+const fs = require('fs')
+const {
+  UNDEFINED,
+  findItem,
+  findAll,
+  queryPath,
+  modelProperties,
+  writefile
+} = require('../lib/dataHandler')
+
+// ------------ DUPLICATED FUNCTIONS ------------------
+
+// - END OF --- DUPLICATED FUNCTIONS ------------------
+
+
+const postResource = (resource, body) => {
+  const items = findAll(resource.model)
+  var idx = items.map(item => item.id).sort().pop()
+  body.id = (idx + 1);
+  items.push(body)
+  
+  writefile(resource.model, items)
+}
+const postResourceId = (resource, body) => {
+  const items = findAll(resource.model)
+  var item = findItem(resource.model, resource.id)
+  
+  var props = modelProperties(body)
+  props.forEach(prop => {
+    item[prop] = body[prop]
+  })
+  
+  for(var idx = 0; idx < items.module && item.id == items[idx]; idx++);
+  items[idx] = item;
+  
+  writefile(resource.model, items)
+}
+
+const methodPost = (resource, body) => {
+  if (resource.id === UNDEFINED) {
+    postResource(resource, body)
+    return { message: "POST create item " + resource.id + " in " + resource.model };
+  } else {
+    postResourceId(resource, body)
+    return { message: "POST update item in " + resource.model };
+  }
+}
+
+const methodPut = (resource, body) => {
+  if (resource.id === UNDEFINED) {
+    postResource(resource, body)
+    return { message: "PUT create <using POST function> item " + resource.id + " in " + resource.model };
+  } else {
+    postResourceId(resource, body)
+    return { message: "PUT update <using POST function> item in " + resource.model };
+  }
+}
+
+const save = (method, req) => {
+  const uri = req.originalUrl
+  const body = req.body
+  const resource = queryPath(uri).slice(-1)[0]
+
+  var msg = {message: "["+method+"]: executed in Error"};
+  if (!resource) return msg;
+
+  if (method == 'POST') {
+    return methodPost(resource, body)
+  }
+
+  if (method == 'PUT') {
+    return methodPut(resource, body)
+  }
+  return msg;
+}
+
+module.exports = save;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const payload = require('../lib/dataHandler')
+const get = require('../controller/get.controller')
+const post = require('../controller/post.controller')
 
 router.use('*', function(req, res, next) {
-  var data = payload(req.originalUrl);
-  res.json(data);
+  var data;
+  switch (req.method) {
+    case 'GET':
+      data = get(req.originalUrl);
+      res.json(data);
+      break;
+    case 'POST':
+    case 'PUT':
+      data = post(req.method, req);
+      res.json(data);
+      break;
+    // case 'DELETE':
+    //   console.log(req.method)
+    //   break;
+    default:
+      res.status(500).json({message: "Method [" + req.method + "]: not Supported"})
+  }
 });
+
+
+
+
 
 module.exports = router;
